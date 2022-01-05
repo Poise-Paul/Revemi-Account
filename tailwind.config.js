8464055e86c545e5397d36dc1a4243048c3fc91d
Poise-Paul <stargrades0@gmail.com>
2022-01-05 18:29:51 +0100

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        funds: "url ('./Assets/Add Funds-01.png')",
+      },
+      colors: {
+        clifford: "#fff444",
+      },
+    },
+  },
+  plugins: [],
+};
